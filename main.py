cefcb655a6738a33f94dfd8a4675c933d5f02774
Slowbro <squirrelsnake34@gmail.com>
2021-11-17 01:12:19 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import PyPDF2
+import os
+import requests
+
+load_dotenv()
+my_text = ''
+
+with open('PDF/sample.pdf', 'rb') as pdf_object:
+    pdf_reader = PyPDF2.PdfFileReader(pdf_object)
+    pages = pdf_reader.numPages
+    # extracts text from each PDF page, assigns it to a string
+    for i in range(0, pages):
+        page_object = pdf_reader.getPage(i)
+        my_text += page_object.extractText()
+
+
+voicerss = os.environ.get('VOICERSS_API_KEY')
+
+parameters = {
+    'key': voicerss,
+    'src': my_text,
+    'hl': 'en-us',
+    'v': 'Linda',
+    'c': 'MP3',
+}
+
+response = requests.get("http://api.voicerss.org/", params=parameters)
+response.raise_for_status()
+audio = response.content
+# saves as .wav file
+with open('PDF/audio.wav', 'wb') as file:
+    file.write(audio)
